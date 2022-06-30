55c09e765566debbd913e27873b748613ff198f4
limjh5699 <limjh5699@gmail.com>
2022-06-30 10:24:22 +0900

    Upload 1152 - Number of Word

---
+++ c#/1152 - Number of Word/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1152___Number_of_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int count = 1;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == ' ') count++;
+            }
+
+            if(input[0] == ' ') count--;
+            if(input[input.Length - 1] == ' ') count--;
+
+            System.Console.WriteLine(count);
+        }
+    }
+}
