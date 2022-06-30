5c3b9bd61c195081ddb0862d32bc49bffab3d5b6
limjh5699 <limjh5699@gmail.com>
2022-06-30 10:25:09 +0900

    Upload 1085 - Escape from Rectangle

---
+++ c#/1085 - Escape from Rectangle/Program.cs
@@ -0,0 +1,23 @@
+﻿int X, Y, W, H;
+
+string input = Console.ReadLine();
+
+string[] space = input.Split(' ');
+
+X = Int32.Parse(space[0]);
+Y = Int32.Parse(space[1]);
+W = Int32.Parse(space[2]);
+H = Int32.Parse(space[3]);
+
+if (X <= Y) // X가 더 작거나 같을 때
+{
+  if(X < W - X) Console.WriteLine(X < H - Y ? X : H - Y);
+  else if(X > W - X) Console.WriteLine(W - X < H - Y ? W - X : H - Y);
+  else if(X == W - X) Console.WriteLine(X < H - Y ? X : H - Y);
+}
+else if (X > Y) // Y가 더 작을 때
+{
+  if(Y < H - Y) Console.WriteLine(Y < W - X ? Y : W - X);
+  else if(Y > H - Y) Console.WriteLine(H - Y < W - X ? H - Y : W - X);
+  else if(Y == H - Y) Console.WriteLine(Y < W - X ? Y : W - X);
+}
