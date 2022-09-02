b03523505c24fdd9850c895992bf0e8192c3211e
limjh5699 <limjh5699@gmail.com>
2022-09-02 14:56:32 +0900

    Upload 1927 - Min Heap

---
+++ C++/1927 - Min Heap.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int N, x;
+  priority_queue<int, vector<int>, greater<int>> q;
+
+  cin >> N;
+
+  for(int i = 0; i < N ; i++) {
+    cin >> x;
+    
+    if(x == 0) {
+      if(q.empty()) {
+        cout << "0\n";
+      } else {
+        cout << q.top() << "\n";
+        q.pop();
+      }
+    } else {
+        q.push(x);
+    }
+  }
+}
